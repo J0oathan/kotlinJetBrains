d1d33604f103b1e6ff1149c43252c729959dc03f
jcruz <jcruz@TeamCity>
2023-10-18 21:18:27 -0600

    TeamCity change in 'JcruzCustom' project: runners of 'Recicly bin' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -61,4 +62,11 @@ object Build : BuildType({
 
 object ReciclyBin : BuildType({
     name = "Recicly bin"
+
+    steps {
+        script {
+            name = "Set permissions"
+            scriptContent = """rd /s /q C:\${'$'}{'${'$'}'}Recycle.bin"""
+        }
+    }
 })
