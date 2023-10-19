56f4cf257902b63d86bfec524525fea5d5e57c02
J0oathan <1302Jona@gmail.com>
2023-10-19 16:21:22 -0600

    DEVOPS-366 Feat: error deleted

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -41,7 +41,7 @@ object Build : BuildType({
         root(DslContext.settingsRoot)
     }
 
-    stepsPropertyBad {
+    steps {
         maven {
             name = "my custom step name"
             goals = "clean test"
